3d0499aa9fd20b43665f15504f52851bef749b73
Elmurzayev Rahim <elmurzayev.r@outlook.com>
2020-12-20 18:43:59 +0100

    Setting up project

---
+++ 2DGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2DGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
